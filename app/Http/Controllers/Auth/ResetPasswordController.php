63de0ec6c1296202a810a41cd5bef4ee85e939af
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2024-12-10 10:15:23 -0300

    Sobreescrevendo trait de de regras de validacao de reset de password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,4 +26,13 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', 'min:4'],
+        ];
+    }
 }
