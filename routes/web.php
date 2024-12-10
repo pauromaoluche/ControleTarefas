47397ac772015e79279274a2d50992cf14e27c96
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2024-12-10 08:56:58 -0300

    Iniciando template markdown

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TarefaController;
+use App\Mail\MensagemTesteMail;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('tarefa', TarefaController::class)->middleware('auth');
+
+Route::get('/email-teste', function(){
+    return new MensagemTesteMail();
+});
