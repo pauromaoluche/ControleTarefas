49d6705f481e6183300eb2c16518a87734509a24
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2024-12-12 09:33:41 -0300

    Definindo titulos do headings

--- app/Exports/tarefasExport.php
+++ app/Exports/tarefasExport.php
@@ -4,16 +4,22 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class tarefasExport implements FromCollection
+class tarefasExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         //return Tarefa::all();
 
         return auth()->user()->tarefas()->get();
+    }
+
+    public function headings(): array
+    {
+        return ['ID da Tarefa', 'Id do Usuario', 'Tarefa', 'Data limite de conclusão', 'Data de criação', 'Data de atualização'];
     }
 }
